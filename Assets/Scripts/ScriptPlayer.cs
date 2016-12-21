e26799baa08034a7948d630bd33aa72bf94df1d8
Felipe Borges <jfpsb_felipe2@hotmail.com>
2016-12-20 23:04:53 -0300

    primeiro commit

---
+++ Assets/Scripts/ScriptPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptPlayer : MonoBehaviour {
+
+    //public GameObject arma;
+
+    private float movementSpeed = 5.0f;
+    
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * movementSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * Time.deltaTime * movementSpeed;
+        }
+    }
+}
