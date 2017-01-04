6236c3e96c55df87243da38060f35d8cbfbd588b
Felipe Borges <jfpsb_felipe2@hotmail.com>
2017-01-03 23:08:16 -0300

    Menu simples adicionado

---
+++ Assets/Scripts/ControleMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleMenu : MonoBehaviour {
+
+    public void IniciarJogo()
+    {
+        // Carrega cena da fase 1
+    }
+
+    public void FecharGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
